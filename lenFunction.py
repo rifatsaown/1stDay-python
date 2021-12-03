d2fc078a2f53375137ad3a7019b30ff574a6133e
rifatsaown <rifatsaown0@gmail.com>
2021-12-03 21:57:07 +0600

    1st day complite

---
+++ lenFunction.py
@@ -0,0 +1,5 @@
+#  print("Number of letter in your name is: ", len(input("Enter your name: ")))
+
+name = input("Enter your name: ")
+number = len(name)
+print("Number of letter in your name is: ", number)
